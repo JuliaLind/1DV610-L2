c764d4bcccaa1425ba72f04cd39b966b45089c7a
Julia Lind <lind.julia@hotmail.com>
2025-09-20 23:55:00 +0200

    Move code from DataMerger to DataFormatter

--- src/lib/DataFormatter.js
+++ src/lib/DataFormatter.js
@@ -1,6 +1,6 @@
 // temp
 import { DataReader } from './DataReader.js'
-import { DataMerger } from './DataMerger.js'
+// import { DataMerger } from './DataMerger.js'
 
 import { readFile } from 'fs/promises'
 const raw = await readFile(new URL('../../test/json/period.json', import.meta.url));
@@ -11,34 +11,57 @@ export class DataFormatter {
     #reader
     #merger
 
-    constructor(config = {
-        reader: new DataReader(),
-        merger: new DataMerger()
-    }) {
-        this.#reader = config.reader
-        this.#merger = config.merger
+    constructor(reader = new DataReader()) {
+        this.#reader = reader
     }
 
     extractRates(data) {
         this.#reader.setData(data.data)
-        this.#merger.setRates(this.#reader.getRates(data))
-        this.#merger.setAttributes(this.#reader.getAttributes(data))
-        this.#merger.setIds(this.#reader.getIds(data))
-        this.#merger.setDates(this.#reader.getDates(data))
 
-        const mergedData = this.#merger.merge()
+        const rates = this.#reader.getRates()
+        const attributes = this.#reader.getAttributes()
+        const ids = this.#reader.getIds()
+        const dates = this.#reader.getDates()
 
-        return mergedData
 
-        // const values = data.data.dataSets[0].series
-        // const ids = data.data.structure.dimensions.series[1].values
-        // const dates = data.data.structure.dimensions.observation[0].values.map(obj => obj.id)
-        // const multipliers = data.data.structure.attributes.series[2].values.map(obj => obj.value = 10 ** obj.id)
 
-        // console.log(values)
-        // console.log(ids)
-        // console.log(dates)
-        // console.log(multipliers)
+        const merged = []
+
+        for (const rateIndex in rates) {
+            const currentRate = rates[rateIndex]
+
+            const exr = {
+                id: ids[rateIndex],
+                values: []
+            }
+
+            let multiplier = 1
+
+            for (const attrIndex in currentRate.attributes) {
+                const currentAttr = attributes[attrIndex]
+
+                if (currentAttr.id === "UNIT_MULT") {
+                    const currentRateMultiplierIndex = currentRate.attributes[attrIndex]
+                    multiplier = 10 ** Number(currentAttr.values[currentRateMultiplierIndex].id)
+                    break
+                }
+            }
+
+            for (const dateIndex in dates) {
+                const observation = {
+                    date: dates[dateIndex],
+                    value: (Number(currentRate.observations[dateIndex][0]) / multiplier).toFixed(4)
+                }
+
+                exr.values.push(observation)
+            }
+
+            merged.push(exr)
+            console.log(exr.values)
+        }
+
+        return merged
+
     }
 }
 
