d708e68b03a0b545cf29fdd022a5a6720f260ec3
Julia Lind <lind.julia@hotmail.com>
2025-09-28 00:18:46 +0200

    Add unittests for arraysAreEqual function

--- test/lib/functions.spec.js
+++ test/lib/functions.spec.js
@@ -1,17 +1,35 @@
 import { expect } from 'chai'
-import { round } from '../../src/lib/functions.js'
+import { round, arraysAreEqual } from '../../src/lib/functions.js'
 
 describe('functions', () => {
-    const values = [
-        { input: [1.005], expected: 1.01 },
-        { input: [1.004], expected: 1.00 },
-        { input: [1.004, 3], expected: 1.004 },
-        { input: [1.0049, 3], expected: 1.005 }
-    ]
+    describe('round()', () => {
+        const values = [
+            { input: [1.005], expected: 1.01 },
+            { input: [1.004], expected: 1.00 },
+            { input: [1.004, 3], expected: 1.004 },
+            { input: [1.0049, 3], expected: 1.005 }
+        ]
 
-    values.forEach(({ input, expected }) => {
-      it(`round(${input.join(', ')}) should return ${expected}`, () => {
-        expect(round(...input)).to.equal(expected)
-      })
+        values.forEach(({ input, expected }) => {
+            it(`round(${input.join(', ')}) should return ${expected}`, () => {
+                expect(round(...input)).to.equal(expected)
+            })
+        })
     })
+
+    describe('arraysAreEqual()', () => {
+        it ('["PLN", "USD"], ["PLN", "USD"] should be true', () => {
+            expect(arraysAreEqual(['PLN', 'USD'], ['PLN', 'USD'])).to.be.true
+        })
+
+        it ('["PLN", "EUR"], ["EUR", "PLN"] should be true', () => {
+            expect(arraysAreEqual(['PLN', 'EUR'], ['EUR', 'PLN'])).to.be.true
+        })
+
+        it ('["PLN", "EUR"], ["EUR", "USD"] should be false', () => {
+            expect(arraysAreEqual(['PLN', 'EUR'], ['EUR', 'USD'])).to.be.false
+        })
+    })
+
+
 })
