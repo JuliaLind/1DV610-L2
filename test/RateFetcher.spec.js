b4bc4eb6a083693ef55140ff955446f41268d6c3
Julia Lind <lind.julia@hotmail.com>
2025-10-18 21:55:20 +0200

    Add new tests, remove old testfiles

--- test/RateFetcher.spec.js
+++ test/RateFetcher.spec.js
@@ -116,6 +116,57 @@ describe('RateFetcher', () => {
     expect(res).to.deep.equal(exp)
   })
 
+    it('fetchLatest() 5 observations integration OK', async () => {
+    fetchStub.resolves({
+      /**
+       * Mocked fetch response.
+       *
+       * @returns {Promise<object>} - promise containing the fake data object
+       */
+      json: () => Promise.resolve(dataPeriod)
+    })
+    const exp = {
+      DKK: {
+        '2025-02-20': 1.5564,
+        '2025-02-21': 1.5591,
+        '2025-02-24': 1.5597,
+        '2025-02-25': 1.5636,
+        '2025-02-26': 1.5673
+      },
+      PLN: {
+        '2025-02-20': 2.7903,
+        '2025-02-21': 2.7899,
+        '2025-02-24': 2.8075,
+        '2025-02-25': 2.8178,
+        '2025-02-26': 2.8208
+      },
+      EUR: {
+        '2025-02-20': 11.609,
+        '2025-02-21': 11.629,
+        '2025-02-24': 11.6355,
+        '2025-02-25': 11.6638,
+        '2025-02-26': 11.6895
+      },
+      SEK: {
+        '2025-02-20': 1.0397,
+        '2025-02-21': 1.0437,
+        '2025-02-24': 1.0433,
+        '2025-02-25': 1.0465,
+        '2025-02-26': 1.049
+      }
+    }
+
+    const sut = new RateFetcher()
+
+    const currencies = ['DKK', 'PLN', 'EUR', 'SEK']
+
+    const res = await sut.fetchLatest({ currencies }, 5)
+
+    expect(res).to.deep.equal(exp)
+  })
+
+  
+
   it('fetchByPeriod() OK', async () => {
     fetchStub.resolves({
       /**
---
+++ test/lib/api/ApiUrl.spec.js
@@ -0,0 +1,16 @@
+
+
+import { expect, use } from 'chai'
+import { ApiUrl } from '../../../src/lib/api/ApiUrl.js'
+
+
+
+describe('ApiUrl', () => {
+    const sut = new ApiUrl()
+
+    it('getRateRequestUrl() 5 observations OK', () => {
+      const url = sut.getRateRequestUrl(['USD', 'EUR'], { observations: 5 })
+
+      expect(url).to.include('lastNObservations=5')
+    })
+})
